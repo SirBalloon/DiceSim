6c76cda4547f22a7208f641bc09ea699db9abba0
Kay._.Toyo <Kibrahim17@hotmail.com>
2025-06-27 23:19:17 +1000

    Added menu and re-rolling of dice

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 //Main file
 
 // Random Number Generator function
-int randNumGen(){
+int randNumGen(){ 
     srand(time(NULL));
     int randNum = rand() % 6 + 1;
     return randNum;
@@ -14,8 +13,27 @@ int randNumGen(){
 // Main Function 
 int main(){
     //Calculations
-    int DiceRoll = randNumGen();
-    //Output
-    printf("You rolled a %d\n", DiceRoll);
-    return 0; 
+    int choiceNum = 1;
+    int DiceRoll = 0;
+
+    while(choiceNum != 2){
+        printf("Welcome to DiceSim! Your very own roll a dice simulator\n");
+        printf("Press 1 to roll the dice.\t");
+        printf("Press 2 to close the program. \n\n");
+
+        scanf("%d", &choiceNum);
+        switch (choiceNum)
+        {
+        case 1:
+            DiceRoll = randNumGen();
+            printf("You rolled a %d\n\n", DiceRoll);
+            break;
+        case 2:
+            printf("DiceSim turned OFF\n");
+            return 0;
+        default:
+            printf("Option Choice Invalid\n\n");
+            continue;
+        }
+    }
 }
