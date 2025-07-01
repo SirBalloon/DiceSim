b73580a4c65b3d2bd6ff88d7e48ab54d2653a3d9
Kay._.Toyo <Kibrahim17@hotmail.com>
2025-07-01 15:54:28 +1000

    Fixed randomness and rolling too fast

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 //Main file
 
 // Random Number Generator function
 int randNumGen(){ 
-    // Generated Seed
-    srand(time(NULL));
     // Generate and return random dice roll
     int minNum = 1;
     int maxNum = 6;
@@ -16,41 +15,49 @@ int randNumGen(){
     return randNum;
 }
 
-void waitFunc(int Time){
-    clock_t start_time = clock();
-    while (clock() < start_time + Time){
-        printf("waiting for next roll\n\n");
+void waitFunc(int CurrentTime){
+    time_t start_time = time(NULL);
+    int DifferenceT = start_time - CurrentTime;
+    int ConditionT = (CurrentTime + DifferenceT) + 2;
+    while (CurrentTime <= ConditionT){
+        int TimeLeft = (start_time - CurrentTime) + 2;
+        CurrentTime += 1.0;
+        printf("%d seconds left before next possible roll.\n\n", TimeLeft);
     };
 }
 
 // Main Function 
 int main(){
     // Defining variables
+    srand(time(NULL));
     int choiceNum = 1;
     int DiceRoll = 0;
-    int EndTime;
+    double elapsedTime;
 
     while(choiceNum != 2){
         // Menu text
-        int currentTime = clock();
         printf("Welcome to DiceSim! Your very own roll a dice simulator\n");
         printf("Press 1 to roll the dice.\t");
         printf("Press 2 to close the program. \n\n");
         // Gathers user input
+        time_t StartTime = time(NULL);
         scanf("%d", &choiceNum);
         // Switch statement to ensure options available with function 
+        time_t currentTime;
         switch (choiceNum)
         {
         case 1:
             // Rolls Dice
-            EndTime = clock();
-            if (difftime(EndTime, currentTime) <= 250){
-                printf("Rolling dice too fast\n\n");
-                waitFunc(10);
-                break;
-            }else{
+            currentTime = time(NULL);
+            elapsedTime = difftime(currentTime, StartTime);
+            if(elapsedTime > 1.0){
                 DiceRoll = randNumGen();
                 printf("You rolled a %d\n\n", DiceRoll);
+            }
+            else{
+                currentTime = time(NULL);
+                printf("Rolling dice too fast\n\n");
+                waitFunc(currentTime);
             }
             break;
         case 2:
